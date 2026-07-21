866e4dd67a900f282a4f97c305987721f69e0399
simsaidan <123339858+simsaidan@users.noreply.github.com>
2026-07-21 02:39:45 -0700

    migrate measurement xor types to decoders repo (#50)
    * migrate measurement xor types to decoders repo
    
    * add type inference
    
    * formtting
    
    * remove custom typeinfer
    
    ---------
    
    Co-authored-by: Aidan Sims <ans248@cornell.edu>
    Co-authored-by: David Plankensteiner <david-pl@users.noreply.github.com>

---
+++ test/test_measurement_xor_typeinfer.py
@@ -0,0 +1,27 @@
+from kirin import passes, types
+from kirin.prelude import structural_no_opt
+
+from bloqade.decoders.dialects.annotate.types import (
+    MeasurementResult,
+    MeasurementResultType,
+)
+
+
+def test_measurement_xor_typeinfer():
+    @structural_no_opt
+    def xor_results(a: MeasurementResult, b: MeasurementResult):
+        return a ^ b
+
+    passes.TypeInfer(structural_no_opt)(xor_results)
+
+    assert xor_results.return_type.is_structurally_equal(MeasurementResultType)
+
+
+def test_int_xor_typeinfer_unchanged():
+    @structural_no_opt
+    def xor_int(a: int, b: int):
+        return a ^ b
+
+    passes.TypeInfer(structural_no_opt)(xor_int)
+
+    assert xor_int.return_type.is_subseteq(types.Int)
