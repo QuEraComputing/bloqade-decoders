f2d46b69cb11efe8fea097be1c3a6f0caa89e8d5
Jason Han <jhan@quera.com>
2026-07-29 11:10:37 -0400

    docs: addressed typing fixes from John's comments

--- demo/decoders_api_demo.py
+++ demo/decoders_api_demo.py
@@ -16,11 +16,11 @@
 # # Getting Started
 # This is a short demo for the interfaces for decoders defined in `bloqade-decoders` and how to use them.
 #
-# We will cover two concrete implementations of the decoders: a lookup-table decoder (`TableDecoder`) that constructs a table from each detector pattern to the frequency of the observable corrections, and a most-likely error (MLE) decoder (`GurobiDecoder`) that solves for the most likely error that triggered the detector flips.
+# We will cover two concrete implementations of the decoders: a lookup-table decoder (`TableDecoder`) that constructs a table from each detector pattern to the frequency of the observable corrections, and a Most-Likely Error (MLE) decoder (`GurobiDecoder`) that solves for the most likely error that triggered the detector flips.
 
 # %% [markdown]
-# ## Constructing decoders
-# To construct a decoder, you pass in the `stim.DetectorErrorModel`. Depending on the decoder, you can optionally pass in arguments to initialize the decoder.
+# ## Constructing Decoders
+# To construct a decoder, you pass in a `stim.DetectorErrorModel` object. Depending on the decoder, you can optionally pass in additional arguments to initialize the decoder.
 
 # %%
 # Define a stim detector error model used for decoding
@@ -49,21 +49,21 @@ mle_decoder = GurobiDecoder(demo_dem)
 # > By default, the `TableDecoder` uses 10,000 shots for training, and the `GurobiDecoder` has `verbose` set to False.
 
 # %%
-lookup_table_decoder_1millionshots = TableDecoder(demo_dem, num_shots=1_000_000)
+lookup_table_decoder_1_million_shots = TableDecoder(demo_dem, num_shots=1_000_000)
 
 # %%
 mle_decoder_verbose = GurobiDecoder(demo_dem, verbose=True)
 
 # %% [markdown]
 # ## Performing decoding
-# Each decoder defines a `decode` method, which takes in a numpy array of detector bits. You can supply an array of bits in for one detector pattern, or you can supply a batch of detectors.
+# Each decoder defines a `decode` method, which takes in a numpy array of booleans as detector bits. You can supply an array of booleans in for one detector pattern, or you can supply a batch of detector patterns.
 
 # %%
 # Use numpy for defining some mock detector patterns
 import numpy as np
 
 # %%
-lookup_table_correction = lookup_table_decoder_1millionshots.decode(
+lookup_table_correction = lookup_table_decoder_1_million_shots.decode(
     detector_bits=np.array([True, True])
 )
 
@@ -80,7 +80,7 @@ print(mle_correction)
 
 # %%
 # We can also get corrections in batches by supplying multiple detector patterns.
-lookup_table_correction_batched = lookup_table_decoder_1millionshots.decode(
+lookup_table_correction_batched = lookup_table_decoder_1_million_shots.decode(
     detector_bits=np.array([[True, True], [False, True]])
 )
 
@@ -102,15 +102,15 @@ print(mle_correction_batched)
 
 # %% [markdown]
 # ## Obtaining Confidence from Decoding
-# Using the `decode_confidence(detectors)` method, you can additionally obtain a confidence value regarding how "confident" you are in your decoding. Understanding this confidence score will vary based on the decoder, but generally the scores will be in the interval `[0.0, 1.0]` where a higher value indicates a higher confidence in decoding.
+# Using the `decode_confidence(detectors)` method, you can additionally obtain a confidence value with your decoding result. Understanding this confidence score will vary based on the decoder, but generally the scores will be in the interval `[0.0, 1.0]` where a higher value indicates a higher confidence in decoding.
 # > For the TableDecoder, the confidence for a given observable correction is computed by the fraction of shots seen for that correction divided by the total number of shots seen for that detector; for the GurobiDecoder, the confidence is a normalized ratio of the probability of the most likely error and the second most likely error.
 #
-# The inputs for `decode_confidence` is the same as `decode`. `decode_confidence` additionally returns a float or array of floats representing the confidence score for each detector.
+# The inputs for `decode_confidence` are the same as `decode`. `decode_confidence` additionally returns a float or a numpy array of floats representing the confidence score for each detector.
 # > The default implementation of `decode_confidence` returns all corrections as equally confident (1.0).
 
 # %%
 lookup_table_correction_confidence = (
-    lookup_table_decoder_1millionshots.decode_confidence(
+    lookup_table_decoder_1_million_shots.decode_confidence(
         detector_bits=np.array([True, True])
     )
 )
@@ -130,7 +130,7 @@ print(mle_correction_confidence)
 
 # %%
 lookup_table_correction_confidence_batch = (
-    lookup_table_decoder_1millionshots.decode_confidence(
+    lookup_table_decoder_1_million_shots.decode_confidence(
         detector_bits=np.array([[True, True], [False, True]])
     )
 )
